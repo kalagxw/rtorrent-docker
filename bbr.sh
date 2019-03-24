9c5a2579086c2cdd18e7dd86a19f4d11f3d3075c
kalagxw <kalagxw@gmail.com>
2019-03-25 07:18:22 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -28,7 +28,7 @@ HAPROXY_LKL_SERVICE_FILE_DEBIAN_URL="${BASE_URL}/startup/haproxy-lkl.init.debain
 HAPROXY_LKL_SERVICE_FILE_REDHAT_URL="${BASE_URL}/startup/haproxy-lkl.init.redhat"
 HAPROXY_LKL_SYSTEMD_FILE_URL="${BASE_URL}/startup/haproxy-lkl.systemd"
 LKL_LIB_URL="https://github.com/kalagxw/bbrovz/raw/master/liblkl-hijack.so.tar"
-LKL_LIB_MD5='606c30c877453d7ad5ad7daf307e4bf3'
+LKL_LIB_MD5='a862d253f123ef07b9595125ffa12e81'
 
 # 需要 BBR 加速的端口
 ACCELERATE_PORT=
